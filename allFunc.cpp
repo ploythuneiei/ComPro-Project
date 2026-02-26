af20f42d2af2c61502eed47b4610cad78a0416d0
aaatommotaaa <105914171+aaatommotaaa@users.noreply.github.com>
2026-02-26 16:07:26 +0700

    Draft2

--- allFunc.cpp
+++ allFunc.cpp
@@ -1,32 +1,47 @@
 #include "allFunc.h"
 
 int main(){
-    seat a;
-    User b;
-    Cinema c;
-    b.showMovieList(c);
-    int choice = b.selectMovie(c);
-    b.showShowtime(c, choice);
-    b.selectShowTime(c, choice);
-
-    showSeatPlan(a);
-    cout << "Select seat: ";
-    cin >> a.row >> a.col;
-
-    while (true){
-    if (isSeatAvailable(a)){
-        cout << "Seat available\n";
-        bookSeat(a);
-        cout << "Booking successful\n";
-        break;
-    }
-    else{
-        cout << "Seat not available\n";
-        cout << "Please enter new seat: ";
-        cin >> a.row >> a.col;
-    }
-}
-    double price = calculatePrice(a);
-    showReceipt(a, price);
+    int login;
+    cout << "Would you like to log-in as a/an...?\n";
+    cout << "[1] admin, [2] user :";
+    cin >> login;
     
+    if(login==1){
+        Admin admin;
+        if(admin.adminlogin()) adminMenu();
+        else cout << "Wrong password\n";
+    } else if (login==2){
+        seat a;
+        User b;
+        Cinema c;
+
+        b.showMovieList(c);
+        int choice = b.selectMovie(c);
+        a.movie = choice;
+        b.showShowtime(c, choice);
+        b.selectShowTime(c, choice);
+
+        showSeatPlan(a);
+        cout << "Select your seat\n Enter row: ";
+        cin >> a.row;
+        cout << "Enter column: ";
+        cin >> a.col;
+
+        while (true){
+            if (isSeatAvailable(a)){
+                cout << "Seat available\n";
+                bookSeat(a);
+                cout << "Booking successful\n";
+                break;
+            } else {
+                cout << "Seat not available\n";
+                cout << "Please enter new seat: ";
+                cin >> a.row >> a.col;
+            }
+        }
+
+        double price = calculatePrice(a);
+        showReceipt(a, price);
+
+    }
 }
--- allFunc.h
+++ allFunc.h
@@ -1,12 +1,19 @@
 #include<iostream>
 #include<string>
-#include <iomanip>
+#include<iomanip>
+#include<cmath>
 #include<vector>
 using namespace std;
 
 int row=7,col=7;
-int booked=0;
 bool seats[10][10][7][7];
+
+const int MAX_MOVIE = 10;
+const int OPEN_TIME = 600;   // เปิด10:00
+const int CLOSE_TIME = 1440; // ปิด 24:00
+const int BREAK_TIME = 30; // พัก 30 นาที
+
+vector<Movie> movies;
 
 struct seat{
     int movie;
@@ -19,16 +26,19 @@ struct seat{
 class Movie{
     public:
         string title;
+        double ticketPrice;
         int duration;
         string Showtime[5];
+        vector<int> showtimes;
         int timecount=0;
         void Display();
+        seat seats;
 };
 
 class Cinema{
     public:
         Movie allMovie[10];
-        int movieCount=0; //เชื่อมกับ vect movie ของพลอย
+        int movieCount=0;
         void showMovie();
 };
 
@@ -43,6 +53,127 @@ class User{
     int countShowtime(Cinema &c,int movieIdx);
     void booking(Cinema &c);
 };
+
+class Admin{
+    private:
+        const string pass = "DoBeforeDebug";
+
+    public:
+        bool adminlogin();
+};
+
+bool Admin::adminlogin(){
+    string input;
+    cout << "Please enter password :";
+    cin >> input;
+    if(input == pass) return true;
+    else return false;
+}
+
+int selectMovieForAdmin(){
+    if(movies.size() == 0){
+        cout << "No movie available\n";
+        return -1;
+    }
+
+    for(int i = 0; i < movies.size(); i++){
+        cout << i+1 << ". " << movies[i].title << endl;
+    }
+
+    int choice;
+    cout << "Select movie: ";
+    cin >> choice;
+
+    if(choice < 1 || choice > movies.size()){
+        cout << "Invalid choice\n";
+        return -1;
+    }
+
+    return choice - 1;
+}
+
+void addMovie(){
+    if(movies.size() >= MAX_MOVIE){
+        cout << "Cannot add more movies\n";
+        return;
+    }
+
+    Movie m;
+    cout << "Enter movie index: ";
+    cin >> m.seats.movie;
+    cout << "Enter movie title: ";
+    getline(cin >> ws, m.title);
+    cout << "Enter ticket price: ";
+    cin >> m.ticketPrice;
+    cout << "Enter movie duration(minutes): ";
+    cin >> m.duration;
+    movies.push_back(m);
+    cout << "Movie added successfully\n";
+    addShowtime(movies.size() - 1);
+    resetSeat(m.seats);
+}
+
+void editTicketPrice(int movieIndex){
+
+    if(movieIndex < 0 || movieIndex >= movies.size()){
+        cout << "Invalid movie index\n";
+        return;
+    }
+
+    double price;
+    cout << "Please input new ticket price: ";
+    cin >> price;
+
+    movies[movieIndex].ticketPrice = price;
+
+    cout << "Ticket price updated successfully\n";
+}
+
+void addShowtime(int movieIndex){
+    movies[movieIndex].showtimes.clear();
+
+    int currentTime = OPEN_TIME;
+
+    while(currentTime + movies[movieIndex].duration + BREAK_TIME <= CLOSE_TIME){
+        movies[movieIndex].showtimes.push_back(currentTime);
+        currentTime = currentTime + movies[movieIndex].duration + BREAK_TIME;
+    }
+
+    cout << "Showtimes added: \n";
+    for(int i = 0; i < movies[movieIndex].showtimes.size(); i++){
+        printTime(movies[movieIndex].showtimes[i]);
+        cout << endl;
+    }
+}
+
+void printTime(int minute){
+    int hour = minute / 60;
+    int min = minute % 60;
+
+    if(min < 10)
+        cout << hour << ":0" << min;
+    else
+        cout << hour << ":" << min;
+}
+
+void adminMenu(){
+    while(true){
+        int input;
+        cout << "1.Add movie\n2.Edit ticket price\n3.Add showtime\n4.Log out\n";
+        cin >> input;
+
+        if(input == 1) addMovie();
+        else if(input == 2){
+            int idx = selectMovieForAdmin();
+            if(idx != -1) editTicketPrice(idx);
+        }
+        else if(input == 3){
+            int idx = selectMovieForAdmin();
+            if(idx != -1) addShowtime(idx);
+        }
+        else break;
+    }
+}
 
 void Movie::Display(){
     cout<<"Title: "<<title<<"\n";
@@ -129,43 +260,42 @@ void showSeatPlan(seat a){
 }
 
 bool isSeatAvailable(seat a){
-    if (a.row < 0 || a.row >= row)
-        return false;
+    if (a.row < 0 || a.row >= row) return false;
+    if (a.col < 0 || a.col >= col) return false;
 
-    if (a.col < 0 || a.col >= col)
-        return false;
-
-    if (a.booked == false)
+    if (seats[a.movie][a.showtime][a.row][a.col] == false)
         return true;
     else
         return false;
 }
 
 void bookSeat(seat &a){
+    seats[a.movie][a.showtime][a.row][a.col] = true;
     a.booked = true;
 }
 
-void showBookingSum(seat a){
-    int available=0;
-    for(int i=0;i<row;i++){
+int showBookingSum(seat a){
+    int available=0,booked=0;
+    for(int i=0;i<row*col;i++){
         for(int j=0;j<col;j++){
             if(!seats[a.movie][a.showtime][i][j]) available+=1;
             else booked+=1;
         }
     }
-    cout << "Available seat: " << available << "\nBooked seat: " << booked;
+    return booked;
 }
 
 double calculatePrice(seat a){
-    showBookingSum(a);
-    
+    Movie m;
+    int amount = showBookingSum(m.seats);
+    return (double) m.ticketPrice * amount;
 }
 
 void showReceipt(seat a, double price){
     double total;
 
     if (a.booked == true){
-        total = price;   // คำนวณราคารวม
+        total = price;
 
         cout << "----------- RECEIPT -----------\n";
         cout << "Movie No     : " << a.movie << endl;
